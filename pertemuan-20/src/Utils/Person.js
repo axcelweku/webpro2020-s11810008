91636dd3a263bd5d194c4fdd96ed3498a9f57c61
axcelweku <axcelw@gmail.com>
2020-11-10 23:33:42 +0800

    pertemuan_20

---
+++ pertemuan-20/src/Utils/Person.js
@@ -0,0 +1,20 @@
+export const person = [
+{
+    name: "Kim Sojung",
+    institution: "Unklab",
+    address: "Tondano",
+    phone: "081xxxxxxxxx"
+},
+{
+    name: "Jung Yerin",
+    institution: "Unklab",
+    address: "Manado",
+    phone: "085xxxxxxxxx"
+},
+{
+    name: "Choi Yuna",
+    institution: "Unklab",
+    address: "Bitung",
+    phone: "082xxxxxxxxx"
+},
+];
